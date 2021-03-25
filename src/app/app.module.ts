f7e4a54ddb534435cdc326c9fb0b44ee97412166
BHARATH GORENTAL <bharathgorental@gmail.com>
2021-03-25 20:19:18 +0530

    Redirect and Wild Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,21 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
  const appRoutes : Routes = [
    {path: '', component: HomeComponent},
-   {path: 'users', component: UsersComponent},
-   {path: 'servers', component: ServersComponent},
+   {path: 'users', component: UsersComponent, children: [
+    {path: ':id/:name', component: UserComponent}
+   ]},
+   {path: 'servers', component: ServersComponent, children:[
+    {path: ':id/edit', component: EditServerComponent},
+    {path: ':id', component: ServerComponent},
+   ]},
+  //  {path: '**', component: PageNotFoundComponent}
+  {path: 'notfound', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/notfound'}
  ];
 
 @NgModule({
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
+
 
 @Component({
   selector: 'app-edit-server',
@@ -11,17 +13,25 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    this.route.queryParams.subscribe((queryParams : Params) =>{
+      this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+    })
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+  }
+
+  onEditServer(id: number){
+    this.router.navigate(['/servers', 5, 'edit'], {queryParams : {allowEdit : 1}, fragment:'loading'})
   }
 
 }
